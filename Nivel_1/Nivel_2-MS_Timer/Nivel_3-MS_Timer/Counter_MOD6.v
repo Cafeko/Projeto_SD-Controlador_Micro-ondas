3fc875154e67324d0d694fe6e8ae21ef957e657c
caio fgs <caiofgs2329@gmail.com>
2023-04-14 22:19:54 -0300

    Counter MOD 6

---
+++ Nivel_1/Nivel_2-MS_Timer/Nivel_3-MS_Timer/Counter_MOD6.v
@@ -0,0 +1,46 @@
+module Counter_MOD6 (
+    input wire loadn, clrn, clock, enable,
+    input wire [3:0] data,
+    output reg [3:0] ones, 
+    output reg tc, zero
+);
+
+    initial 
+    begin
+        ones <= 0;    
+    end
+
+    always @(posedge clock or negedge clrn or negedge loadn) 
+    begin
+        if (!clrn)
+            ones <= 4'b0000;
+        else
+        begin
+            if (!loadn)
+                ones <= data;
+            else
+            begin
+                if (enable)
+                begin
+                    if (ones == 4'b0000)
+                        ones <= 4'b0101;
+                    else
+                        ones <= ones - 1;
+                end
+            end
+        end
+    end
+
+    always @(ones or enable)
+    begin
+        if (ones == 0)
+            zero <= 1'b1;
+        else
+            zero <= 1'b0;
+        if (ones == 0 && enable)
+                tc <= 1'b1;
+            else
+                tc <= 1'b0;
+    end
+    
+endmodule
--- Nivel_1/Nivel_2-MS_Timer/Nivel_3-MS_Timer/TB/Counter_MOD6_TB/Counter_MOD6_TB.v
+++ Nivel_1/Nivel_2-MS_Timer/Nivel_3-MS_Timer/TB/Counter_MOD6_TB/Counter_MOD6_TB.v
@@ -1,18 +1,18 @@
 `timescale 1ns/1ps
-`include "..\\Counter_MOD10.v"
+`include "..\\..\\Counter_MOD6.v"
 
-module Counter_MOD10_TB ();
+module Counter_MOD6_TB ();
     reg load, clear, clk, enab;
     reg [3:0] numero;
     wire [3:0] numero_saida;
     wire tc_saida, zero_saida;
 
-    Counter_MOD10 dut (load, clear, clk, enab, numero, numero_saida, tc_saida, zero_saida);
+    Counter_MOD6 dut (load, clear, clk, enab, numero, numero_saida, tc_saida, zero_saida);
 
     initial 
     begin
-        $dumpfile("Counter_MOD10_TB.vcd");
-        $dumpvars(0,Counter_MOD10_TB);
+        $dumpfile("Counter_MOD6_TB.vcd");
+        $dumpvars(0,Counter_MOD6_TB);
 
         // Conta
         #0 load <= 1; clear <= 1; clk <= 0; enab <= 0; numero <= 4'b0000;
@@ -31,34 +31,20 @@ module Counter_MOD10_TB ();
         #1 load <= 1; clear <= 1; clk <= 0; enab <= 1; numero <= 4'b0000;
         #1 load <= 1; clear <= 1; clk <= 1; enab <= 1; numero <= 4'b0000;
         #1 load <= 1; clear <= 1; clk <= 0; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 1; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 0; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 1; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 0; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 1; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 0; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 1; enab <= 1; numero <= 4'b0000;
-        
 
         #1 load <= 1; clear <= 0; clk <= 0; enab <= 0; numero <= 4'b0000;
         #1 load <= 1; clear <= 1; clk <= 0; enab <= 0; numero <= 4'b0000;
 
         // Teste load e tc
-        #1 load <= 0; clear <= 1; clk <= 0; enab <= 0; numero <= 4'b0101;
+        #1 load <= 0; clear <= 1; clk <= 0; enab <= 0; numero <= 4'b0011;
         #1 load <= 1; clear <= 1; clk <= 0; enab <= 1; numero <= 4'b0000;
         #1 load <= 1; clear <= 1; clk <= 1; enab <= 1; numero <= 4'b0000;
         #1 load <= 1; clear <= 1; clk <= 0; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 1; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 0; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 1; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 0; enab <= 1; numero <= 4'b0000;
-        #1 load <= 1; clear <= 1; clk <= 1; enab <= 1; numero <= 4'b0000;
+        #1 load <= 1; clear <= 1; clk <= 1; enab <= 1; numero <= 4'b0100;
         #1 load <= 1; clear <= 1; clk <= 0; enab <= 1; numero <= 4'b0000;
         #1 load <= 1; clear <= 1; clk <= 1; enab <= 1; numero <= 4'b0000;
         #1 load <= 1; clear <= 1; clk <= 0; enab <= 0; numero <= 4'b0000;
         #1 load <= 1; clear <= 1; clk <= 1; enab <= 0; numero <= 4'b0000;
-        
-
         
         #2;
 
